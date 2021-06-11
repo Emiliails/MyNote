c39a83de40a913e9a0729a995ff7b3bcd8f6d933
niubo <niubo60@icloud.com>
2021-06-11 16:56:40 +0800

    jump between main page and add note page

--- app/src/main/java/com/niubo/mynote/CreateNoteActivity.java
+++ app/src/main/java/com/niubo/mynote/CreateNoteActivity.java
@@ -3,6 +3,8 @@ package com.niubo.mynote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Called when the activity has detected the user's press of the back key.
+                onBackPressed();
+            }
+        });
     }
 }
